57843ffdd137ba64142fd27b79ec0bb0fb8f5a78
Iida <iida.hinkkanen@helsinki.fi>
2020-01-20 01:01:36 +0200

    renaming variables & folders + bootstrap

--- application/recipes/models.py
+++ application/recipes/models.py
@@ -1,14 +1,14 @@
 from application import db
 
-class Task(db.Model):
+class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
     onupdate=db.func.current_timestamp())
 
-    name = db.Column(db.String(144), nullable=False)
-    done = db.Column(db.Boolean, nullable=False)
+    header = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.String(144), nullable=False)
 
-    def __init__(self, name):
-        self.name = name
-        self.done = False
+    def __init__(self, header, content):
+        self.header = header
+        self.content = content
