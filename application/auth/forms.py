75866b6bfa33dc5d8af6fcbbe5727b6f4ddc62af
Iida <iida.hinkkanen@helsinki.fi>
2020-01-29 22:35:29 +0200

    registration enabled

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -7,3 +7,10 @@ class LoginForm(FlaskForm):
   
     class Meta:
         csrf = False
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name")
+    password = PasswordField("Password")
+  
+    class Meta:
+        csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user
 
 from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -25,4 +25,28 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
-    return redirect(url_for("index")) 
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form = RegisterForm())
+        
+    form = RegisterForm(request.form)
+    # mahdolliset validoinnit
+
+    
+    u = User.query.filter_by(name=form.name.data).first()
+
+    if not u:
+        user = User(form.name.data, form.password.data)
+        db.session().add(user)
+        db.session().commit()
+        
+        return redirect(url_for("index"))
+    
+    return render_template("auth/registerform.html", form = form,
+                               error = "Name already registered")
+
+
