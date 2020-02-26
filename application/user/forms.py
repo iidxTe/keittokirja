1928d4d7e199e359dbd05fa0cb17c9bbc67abe54
Iida <iida.hinkkanen@helsinki.fi>
2020-02-26 22:27:29 +0200

    user name & password editing enabled

--- application/user/forms.py
+++ application/user/forms.py
@@ -4,8 +4,8 @@ from wtforms import StringField, ValidationError, validators
 #LISÄÄ VALIDAATTOREITA
 
 class EditUserForm(FlaskForm):
-    header = StringField("Muokkaa nimeä", [validators.InputRequired])
-    category = StringField("Muokkaa salasanaa", [validators.InputRequired])
+    name = StringField("Muokkaa nimeä", [validators.InputRequired])
+    password = StringField("Muokkaa salasanaa", [validators.InputRequired])
  
     class Meta:
         csrf = False
--- application/user/views.py
+++ application/user/views.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for
-from flask_login import login_required
+from flask import render_template, url_for, redirect, request
+from flask_login import login_required, current_user
 
 from application import app, db
 
@@ -10,7 +10,41 @@ from application.user.forms import EditUserForm
 @app.route("/user/<user_id>/", methods=["GET"])
 @login_required
 def go_to_user_info(user_id):
+
+    user = User.query.get(user_id)
     
-    return render_template("user/userinfo.html")
+    return render_template("user/userinfo.html", user=user)
+
+
+@app.route("/user/edit/<user_id>/", methods=["GET", "POST"])
+@login_required
+def edit_user(user_id):
+
+    user = User.query.get(user_id)
+
+    if request.method == "GET":
+        form = EditUserForm()
+
+        form.name.data = user.name
+        form.password.data = user.password
+    
+        return render_template("user/edit.html", form=form, user=user)
+
+
+    form = EditUserForm(request.form)
+    user.account_id = current_user.id
+
+    '''
+    if not form.validate():
+        return render_template("user/edit.html", form = form, user=user)
+    '''
+
+    user.name = form.name.data
+    user.password = form.password.data
+
+    db.session().add(user)
+    db.session().commit()
+
+    return redirect(url_for("go_to_user_info", user_id = user.account_id))
 
 
